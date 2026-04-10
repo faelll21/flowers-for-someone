212e27ce289027ec5582a2f4bfb68629439587e0
faelll21 <rafaelyohanis07@gmail.com>
2026-04-11 00:24:05 +0800

    Update greeting text from 'Meilaa' to 'Gracee'

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Hi Meilaa I Have Something`.split('')
+const text = `Hi Gracee I Have Something`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
